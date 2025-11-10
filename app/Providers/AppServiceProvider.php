2e0f2fef382a5eed0bb06da508a52aa4610c7db6
Sinh Ho <sinh.ho@hotmail.com>
2025-11-10 09:40:11 +0000

    Add initial application structure and features
    - Create privacy policy and terms of service markdown files.
    - Implement main application layout in Blade template.
    - Add team invitation email template.
    - Define API routes for user authentication.
    - Configure Tailwind CSS with custom fonts and plugins.
    - Develop feature tests for API token permissions, authentication, and user account management.
    - Implement tests for email verification, password confirmation, and password reset functionalities.
    - Add tests for profile information updates and user registration.
    - Create tests for two-factor authentication settings and password updates.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Inertia::share([
+            'nav' => [
+                ['name' => 'Dashboard', 'route' => 'dashboard', 'group' => 'dashboard'],
+            ],
+        ]);
     }
 }
